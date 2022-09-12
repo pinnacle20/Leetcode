a0545baf7ce4155400983af33b976edf5d3354de
pinnacle20 <73813570+pinnacle20@users.noreply.github.com>
2022-09-12 23:42:15 +0530

    Time: 27 ms (59.80%), Space: 12.6 MB (35.24%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target)
+    {
+    int n=nums.size();
+ 
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        if((sum+target)%2!=0) return 0;
+        int m=(sum+target)/2;
+        if(m<0) return 0;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        dp[0][0]=1;
+        for(int i=1;i<n+1;i++){
+            if(nums[i-1]==0) dp[i][0]=2*dp[i-1][0];
+            else dp[i][0]=dp[i-1][0];
+            // cout<<dp[i][0]<<endl;
+        }
+        
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<m+1;j++)
+            {
+                if(nums[i-1]>j)
+                    dp[i][j]=dp[i-1][j];
+                else 
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];
+            }
+        }
+        
+       return dp[n][m]; 
+        
+        
+    }
+};
